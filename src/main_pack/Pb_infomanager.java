c0f4fb2c83178cbbb836838145f55cc6e350149d
hyun1014 <ksp70712@gmail.com>
2020-04-05 17:05:27 +0900

    Added exception handling

--- src/main_pack/Pb_infomanager.java
+++ src/main_pack/Pb_infomanager.java
@@ -1,6 +1,5 @@
 package main_pack;
-import java.util.TreeMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Pb_infomanager {
 	static Pb_infomanager m = null;
@@ -22,14 +21,21 @@ public class Pb_infomanager {
 	}
 	void newInfo() {
 		String name, phoneNum, dept;
-		int age, year;
+		int age = -1, year = -1;
 		int ch = -1;
 		while(ch<0 || ch>3) {
 			System.out.println("Select person type:\n1.Company / 2.University / 3.Default / 0.Exit");
-			ch = sc.nextInt();
-			sc.nextLine();
-			if(ch<0 && ch>3)
-				System.out.println("Invalid choice.");
+			try {
+				ch = sc.nextInt();
+				sc.nextLine();
+				if(ch<0 || ch>3)
+					System.out.println("Invalid choice.\n");
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Invalid input. Only number is valid.\n");
+				sc.nextLine();
+				ch = -1;
+			}
 		}
 		if(ch==0) {
 			System.out.println("Canceled.");
@@ -40,25 +46,59 @@ public class Pb_infomanager {
 		name = sc.nextLine();
 		System.out.print("Phone num: ");
 		phoneNum = sc.nextLine();
-		System.out.print("Age: ");
-		age = sc.nextInt();
-		sc.nextLine();
+		while(age<0) {
+			try {
+				System.out.print("Age: ");
+				age = sc.nextInt();
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Invalid input. Only number is valid.");
+				age = -1;
+			}
+			finally {
+				sc.nextLine();
+			}
+			if(age<0)
+				System.out.println("Invalid value.");
+		}
 		if(ch==3)
 			db.put(name, new Person(name, phoneNum, age));
 		else if(ch==1) {
 			System.out.print("Department: ");
 			dept = sc.nextLine();
-			System.out.print("Experience year: ");
-			year = sc.nextInt();
-			sc.nextLine();
+			while(year<0) {
+				try {
+					System.out.print("Experience year: ");
+					year = sc.nextInt();
+				}
+				catch(InputMismatchException e) {
+					System.out.println("Invalid input. Only number is valid.");
+					year = -1;
+				}
+				finally {
+					sc.nextLine();
+				}
+				if(year<0)
+					System.out.println("Invalid value.");
+			}
 			db.put(name, new Person_com(name, phoneNum, age, dept, year));
 		}
 		else {
 			System.out.print("Department: ");
 			dept = sc.nextLine();
-			System.out.print("Year: ");
-			year = sc.nextInt();
-			sc.nextLine();
+			while(year<0) {
+				try {
+					System.out.print("Year: ");
+					year = sc.nextInt();
+				}
+				catch(InputMismatchException e) {
+					System.out.println("Invalid input. Only number is valid.");
+					year = -1;
+				}
+				finally {
+					sc.nextLine();
+				}
+			}
 			db.put(name, new Person_univ(name, phoneNum, age, dept, year));
 		}
 		System.out.println("Info created.");
@@ -85,16 +125,22 @@ public class Pb_infomanager {
 		Person tar = searchInfo();
 		if(tar==null)
 			return;
-		System.out.println("Do you want to delete info? [Y/N]");
-		ch = sc.nextLine().charAt(0);
-		System.out.println();
-		if(ch=='y'||ch=='Y') {
-			db.remove(tar.getName());
-			System.out.println("Deleted.");
+		while(true) {
+			System.out.println("Do you want to delete info? [Y/N]");
+			ch = sc.nextLine().charAt(0);
+			System.out.println();
+			if(ch=='y'||ch=='Y') {
+				db.remove(tar.getName());
+				System.out.println("Deleted.");
+				break;
+			}
+			else if(ch=='n'||ch=='N') {
+				System.out.println("Canceled.");
+				break;
+			}
+			else
+				System.out.println("Invalid choice.");
 		}
-		else if(ch=='n'||ch=='N')
-			System.out.println("Canceled.");
-		else
-			System.out.println("Invalid choice.");
+		
 	}
 }
